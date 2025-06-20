b595bd7f298d5f6775a1a14931509240f70951c9
muhammad <fajzievmuhammad09@gmail.com>
2025-06-20 11:17:55 +0500

    modified:   src/app/components/Header.jsx 	modified:   src/app/globals.css 	modified:   src/app/page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import HomePage from "./components/HomePage";
 
 export default function Home() {
   return (
     <div>
-      hello  worlad
+      <HomePage/>
     </div>
   );
 }
