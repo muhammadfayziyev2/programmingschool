47745c4b99428a382bf9f0e631db471b6ad5b909
ismoil4944 <oismoiljon08@gmail.com>
2025-06-20 11:27:30 +0500

    renamed:    public/img/logo 1.png -> public/logo1.png 	modified:   src/app/components/Header.jsx 	modified:   src/app/globals.css

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,19 +4,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-<<<<<<< HEAD
     <div>
       <div className="header">
         <div className="header_contaner">
-          <div className="logo_name"></div>
+          <div className="logo_name"><img src="public/logo1.png" alt="" /></div>
           <div className="header_units"></div>
           <div className="header_media"></div>
         </div>
       </div>
     </div>
-=======
-    <div className='header'>Header</div>
->>>>>>> 07059e3571df2262cb4f9182754b4e3b85be7411
   )
 }
 
