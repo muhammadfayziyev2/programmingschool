c809c9505b0943aa8b3b76db5ed4ca57af398fe9
ismoil4944 <oismoiljon08@gmail.com>
2025-06-20 11:51:26 +0500

    deleted:    public/logo1.png 	new file:   public/prchool_logo.jpg 	modified:   src/app/components/Header.jsx 	modified:   src/app/globals.css

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -8,10 +8,18 @@ const Header = () => {
       <div className="header">
         <div className="header_contaner">
           <div className="logo_name">
-            <img src="logo1.png" alt="" className='img'/>
+            <img src="prchool_logo.jpg" alt="" className='img'/>
           </div>
-          <div className="header_units"></div>
-          <div className="header_media"></div>
+          <div className="header_units">
+            <ul className='header_unit1'>
+              <li className='header_unit2'>Bosh sahifa</li>
+              <li className='header_unit2'>Biz haqimizda</li>
+              <li className='header_unit2'>Yaratuvchilar</li>
+            </ul>
+          </div>
+          <div className="header_media">
+            
+          </div>
         </div>
       </div>
     </div>
