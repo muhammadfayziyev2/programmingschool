191651ca9b0f5c5a488cd739351cc06b0d43df37
muhammad <fajzievmuhammad09@gmail.com>
2025-06-21 12:12:07 +0500

    new file:   public/IMG_20250414_112944_658 (3).jpg 	new file:   public/uzim.png 	new file:   src/app/components/AboutUs.jsx 	modified:   src/app/components/HomePage.jsx 	modified:   src/app/components/MainPage.jsx 	modified:   src/app/globals.css 	modified:   src/app/layout.js

---
+++ src/app/components/AboutUs.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>
+          <div className="parent">
+              <div className="card">
+                  <div className="content-box">
+                  
+                      <img src="IMG_20250414_112944_658 (3).jpg" alt="" className="card-title" />
+                      <p className="card-content">
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      </p>
+                      <span className="see-more">See More</span>
+                  </div>
+                  <div className="date-box">
+                      <span className="month">JUNE</span>
+                      <span className="date">29</span>
+                  </div>
+              </div>
+          </div>
+    </div>
+  )
+}
+
+export default AboutUs
--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import MainPage from './MainPage'
+import AboutUs from './AboutUs'
 
 const HomePage = () => {
   return (
     <div>
         <Header/>
         <MainPage/>
+        <AboutUs/>
     </div>
   )
 }
